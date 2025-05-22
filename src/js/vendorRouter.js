673642c7f44455ba6fcc2327d9a603e2694ee49c
vinipdiass <quaackyt@gmail.com>
2025-05-22 16:47:59 -0300

    commit final

--- src/js/vendorRouter.js
+++ src/js/vendorRouter.js
@@ -1,12 +1,10 @@
-export async function routeToSeller() {
+window.routeToSeller = async function () {
   try {
     const r = await fetch("http://localhost:3000/seller");
-        // mesma origem ou mude para sua API
     const { link } = await r.json();
     window.open(link, "_blank", "noopener");
   } catch (err) {
     console.error("Erro ao buscar vendedor:", err);
-    // fallback estático se a API falhar
     window.open("https://wa.me/555181331593", "_blank", "noopener");
   }
-}
+};
